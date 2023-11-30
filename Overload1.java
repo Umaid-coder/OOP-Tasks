4306320e2799fd5c18225c12ee5d9970b69d1418
Umaid Ali <81355970+Umaid-coder@users.noreply.github.com>
2023-11-30 09:12:57 -0800

    Add files via upload

---
+++ Overload1.java
@@ -0,0 +1,30 @@
+ class OverloadDemo{
+           void test(){
+		   System.out. println (" No parameters");
+		   }
+		   
+		   
+		   void test( int a, int b){
+			   System.out.println( " a and b:" +a+ " " +b);
+		   }
+		      void test( double a){
+				  System.out.println( " double of a: " + a);
+				
+				
+			}
+ }
+   class Overload1{
+	    public static void main ( String args[]){
+			
+			OverloadDemo ob = new OverloadDemo();
+			
+			int i =88;
+			
+			ob.test();
+			
+			ob.test(10,20);
+			ob.test(i);
+			ob.test(123.2);
+			
+		}
+   }
