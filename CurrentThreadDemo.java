6aa32da7b8ff0f99d8c38d8719406e1560ca051b
Umaid Ali <81355970+Umaid-coder@users.noreply.github.com>
2024-01-17 23:05:45 +0500

    Add files via upload

---
+++ CurrentThreadDemo.java
@@ -0,0 +1,22 @@
+class CurrentThreadDemo{
+	 public static void main(String args[]){
+		 Thread t=Thread.currentThread();
+		 
+		 System.out.println( " Current thread:" +t);
+
+			t.setName("My Thread");
+
+			System.out.println( " after chanege name of  thread:" +t);
+			
+			try{
+				
+				for (int i=5; i>0; i--){
+					System.out.println(i);
+					Thread.sleep(1000);
+				}
+			} catch(InterruptedException e){
+				
+				System.out.println( " Main Thread Interrupted" );
+			}
+	 }
+}	 
